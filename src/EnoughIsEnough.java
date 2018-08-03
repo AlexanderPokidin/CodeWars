106351bb0ab168fb331964a5b02f9fcf8cc27c0a
AlexanderPokidin <a.pokidin@gmail.com>
2018-08-04 00:13:34 +0300

    EnoughIsEnough

--- src/EnoughIsEnough.java
+++ src/EnoughIsEnough.java
@@ -4,29 +4,35 @@ public class EnoughIsEnough {
 
     public static int[] deleteNth(int[] elements, int maxOccurrences) {
 
-        ArrayList<Integer> result = new ArrayList<>();
-        for (int i = 0; i < elements.length; i++) {
-            result.add(elements[i]);
-        }
+        if (elements.length == 0){
+            return elements;
+        } else {
+            ArrayList<Integer> result = new ArrayList<>();
+            for (int i = 0; i < elements.length; i++) {
+                result.add(elements[i]);
+            }
 
-        for (int i = 0; i < result.size(); i++) {
-            int count = 0;
-            for (int j = i; j < result.size(); j++) {
-                if (result.get(i).equals(result.get(j))) {
-                    count++;
-                    if (count > maxOccurrences) {
-                        result.remove(j);
+            for (int i = 0; i < result.size(); i++) {
+                int count = 0;
+                for (int j = i; j < result.size(); j++) {
+                    if (result.get(i).equals(result.get(j))) {
+                        count++;
+                        if (count > maxOccurrences) {
+                            result.remove(j);
+                        }
                     }
                 }
             }
+
+            int[] resArray = new int[result.size()];
+
+            for (int i = 0; i < result.size(); i++) {
+                resArray[i] = result.get(i);
+            }
+            return resArray;
         }
 
-        int[] resArray = new int[result.size()];
 
-        for (int i = 0; i < result.size(); i++) {
-            resArray[i] = result.get(i);
-        }
-        return resArray;
     }
 
     public static void main(String[] args) {
@@ -41,6 +47,13 @@ public class EnoughIsEnough {
         int[] testArray2 = deleteNth(new int[]{ 1, 2, 3, 1, 1, 2, 1, 2, 3, 3, 2, 4, 5, 3, 1 }, 3);
         for (int i = 0; i < testArray2.length; i++) {
             System.out.print(testArray2[i]);
+            System.out.print(" ");
+        }
+        System.out.println("");
+
+        int[] testArray3 = deleteNth(new int[]{ }, 5);
+        for (int i = 0; i < testArray3.length; i++) {
+            System.out.print(testArray3[i]);
             System.out.print(" ");
         }
         System.out.println("");
