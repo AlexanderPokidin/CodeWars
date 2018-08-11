2925b8af1ba310fd5940f94aa9f043cef761eb30
AlexanderPokidin <a.pokidin@gmail.com>
2018-08-11 22:14:50 +0300

    Human Readable Time

---
+++ src/HumanReadableTime.java
@@ -0,0 +1,16 @@
+public class HumanReadableTime {
+    public static String makeReadable(int seconds) {
+        int h = seconds / 3600;
+        int m = (seconds % 3600) / 60;
+        int s = seconds % 60;
+        return String.format("%02d:%02d:%02d", h, m, s);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(makeReadable(0));
+        System.out.println(makeReadable(5));
+        System.out.println(makeReadable(60));
+        System.out.println(makeReadable(86399));
+        System.out.println(makeReadable(359999));
+    }
+}
