5beb12b988befd194c5f807f26b29b6a837d6865
AlexanderPokidin <a.pokidin@gmail.com>
2018-08-15 17:05:13 +0300

    Mexican Wave

---
+++ src/MexicanWave.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class MexicanWave {
+
+    public static String[] wave(String str) {
+        int arrLength = str.length();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ' ') {
+                arrLength--;
+            }
+        }
+
+        String[] result = new String[arrLength];
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            String myStr;
+            if (str.charAt(i) != ' ') {
+                if (i > 0) {
+                    myStr = str.substring(0, i) + str.substring(i, i + 1).toUpperCase() + str.substring(i + 1);
+                } else {
+                    myStr = str.substring(i, i + 1).toUpperCase() + str.substring(i + 1);
+                }
+                result[count] = myStr;
+                count ++;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(wave("hello")));
+        System.out.println(Arrays.toString(wave("Two words")));
+        System.out.println(Arrays.toString(wave(" gap ")));
+
+    }
+}
