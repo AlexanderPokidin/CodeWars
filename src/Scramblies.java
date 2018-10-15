b32c2ab535f57c5a6935e81908111c75cd443d8a
AlexanderPokidin <a.pokidin@gmail.com>
2018-10-15 23:27:50 +0300

    Scramblies

---
+++ src/Scramblies.java
@@ -0,0 +1,38 @@
+public class Scramblies {
+
+    //TODO Задача не решена. Отсутсвет проверка на повторяющиеся буквы
+    // https://www.codewars.com/kata/55c04b4cc56a697bb0000048/train/java
+    public static boolean scramble(String str1, String str2) {
+        if (str1.length() < str2.length()) {
+            return false;
+        } else {
+            int count = 0;
+            char[] chars = str1.toCharArray();
+
+            for (int i = 0; i < str2.length(); i++) {
+                char tmp = str2.charAt(i);
+                for (int j = 0; j < chars.length; j++) {
+                    if (tmp == chars[j]) {
+                        chars[j] = ' ';
+                        count++;
+                        break;
+                    }
+                }
+            }
+            return count == str2.length();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(scramble("rkqodlw", "world"));
+        System.out.println(scramble("cedewaraaossoqqyt", "codewars"));
+        System.out.println(scramble("katas", "steak"));
+        System.out.println(scramble("scriptjavx", "javascript"));
+        System.out.println(scramble("scriptingjava", "javascript"));
+        System.out.println(scramble("scriptsjava", "javascripts"));
+        System.out.println(scramble("javscripts", "javascript"));
+        System.out.println(scramble("aabbcamaomsccdd", "commas"));
+        System.out.println(scramble("commas", "commas"));
+        System.out.println(scramble("sammoc", "commas"));
+    }
+}
